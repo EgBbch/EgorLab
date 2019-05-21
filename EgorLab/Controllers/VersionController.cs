95ffe15eeb1af3fd40c6ab7eb6bc833f151c75bb
Egor <wender754@mail.ru>
2019-05-21 13:44:30 +0700

    VersionController changed

--- EgorLab/Controllers/VersionController.cs
+++ EgorLab/Controllers/VersionController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace EgorLab.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/version")]
     [ApiController]
     public class VersionController : ControllerBase
     {
