e6c87b9b6d62056abf455d84eed904e8c9d22c6b
Egor <wender754@mail.ru>
2019-05-21 13:39:57 +0700

    MemStorage add

---
+++ EgorLab/Models/StorageModels/MemStorage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgorLab.Models.StorageModels
+{
+    public class MemStorage : IStorage<Person>
+    {
+        private readonly List<Person> people;
+
+        public MemStorage()
+        {
+            people = new List<Person>();
+        }
+
+        public IEnumerable<Person> All => people.Select(v => v);
+
+        public Person this[Guid id]
+        {
+            get
+            {
+                return people.FirstOrDefault(v => v.Id == id);
+            }
+            set
+            {
+                var person = people.FirstOrDefault(v => v.Id == id);
+                if (person == null)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                person = value;
+            }
+        }
+
+        public void Add(Person person)
+        {
+            person.Id = Guid.NewGuid();
+            people.Add(person);
+        }
+
+        public bool Has(Guid id)
+        {
+            return people.Any(v => v.Id == id);
+        }
+
+        public void RemoveAt(Guid id)
+        {
+            var person = people.FirstOrDefault(v => v.Id == id);
+            if (person != null)
+            {
+                people.Remove(person);
+            }
+        }       
+    }
+}
+
